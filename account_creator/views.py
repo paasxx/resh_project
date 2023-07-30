ef8b60249c806c95aa1478ffc45935586558d5ed
Pedro <paasxx@gmail.com>
2023-07-30 19:30:17 -0300

    implementing messages, and finishing register view

--- account_creator/views.py
+++ account_creator/views.py
@@ -1,33 +1,36 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
 from django.http import HttpResponse
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate,logout
 from django.contrib.auth.forms import UserCreationForm
 
 
+from django.contrib import messages
+
 # Create your views here.
 
-def registerPage(response):
-    if response.method == "POST":
-        form = RegisterForm(response.POST)
+def registerPage(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get("username")
+            messages.success(request, "Account was created for " +user)
+            return redirect('login')
 
-        else:
-            pass
-            return HttpResponse("Dados inválidos")
-
-        return redirect("home")
-
-    else:
-        form = RegisterForm()
+    
+    form = RegisterForm()
 
     context = { "form":form}
 
-    return render(response, "account_creator/register.html", context)
+    return render(request, "account_creator/register.html", context)
 
 
 def loginPage(request):
+
+    if request.method == "POST":
+        request.POST.get('useranme')
+        request.POST.get('password')
     context = {}
     return render(request, "account_creator/login.html")
 
