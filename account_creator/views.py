2a0644edcdcad744280ef68658d2e627e911d0b8
Pedro <paasxx@gmail.com>
2023-07-30 23:10:40 -0300

    implemented error register

--- account_creator/views.py
+++ account_creator/views.py
@@ -3,6 +3,7 @@ from .forms import RegisterForm
 from django.http import HttpResponse
 from django.contrib.auth import login, authenticate,logout
 from django.contrib.auth.forms import UserCreationForm
+from json import dumps as jdumps
 
 from django.contrib.auth.decorators import login_required
 
@@ -11,43 +12,62 @@ from django.contrib import messages
 # Create your views here.
 
 def registerPage(request):
-    if request.method == "POST":
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            user = form.cleaned_data.get("username")
-            messages.success(request, "Account was created for " +user)
-            return redirect('login')
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == "POST":
+            form = RegisterForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get("username")
+                messages.success(request, "Account was created for " +user)
+                return redirect('login')
+            
+            else:
 
-    
-    form = RegisterForm()
 
-    context = { "form":form}
+                context = {"form":form}
+                
 
-    return render(request, "account_creator/register.html", context)
+                return render(request, "account_creator/register.html", context)
+
+            
+
+        form = RegisterForm()
+
+        context = { "form":form}
+
+        return render(request, "account_creator/register.html", context)
 
 
 def loginPage(request):
 
-    if request.method == "POST":
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+    if request.user.is_authenticated:
+        return redirect('home')
+    
+    else:
 
-        user = authenticate(request, username=username, password=password)
+        if request.method == "POST":
+            username = request.POST.get('username')
+            password = request.POST.get('password')
 
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.info(request, "Username OR password is incorrect!")
+            user = authenticate(request, username=username, password=password)
 
-    context = {}
-    return render(request, "account_creator/login.html")
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, "Username OR password is incorrect!")
+
+        context = {}
+        return render(request, "account_creator/login.html", context)
 
 
 def logoutPage(request):
     logout(request)
     return redirect('login')
 
+
+@login_required(login_url = 'login')
 def home(request):
     return render(request, "account_creator/home.html")
